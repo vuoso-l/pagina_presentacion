db7b668e98c0ffa493b3f4734303195f9884853f
vuoso-l <vuosolucas@gmail.com>
2022-01-11 17:37:21 -0300

    Modified header and return button

--- js/proyectosEstaticos.js
+++ js/proyectosEstaticos.js
@@ -1,53 +1,46 @@
 const btnEstaticos = document.querySelector(".estaticos button");
-const headerEstaticos = document.querySelector("header");
 const h1Estaticos = document.querySelector("h1");
+const h2Estaticos = document.querySelector("h2");
 const h3Estaticos = document.querySelector("h3");
 const containerEstaticos = document.querySelector(".container");
 
 btnEstaticos.addEventListener("click", (e) => {
-    e.preventDefault();
-    h1Estaticos.innerHTML = "Repositorio estático";
-    h3Estaticos.innerHTML = "Basado en aprendizaje Certified Tech Developer";
-    headerEstaticos.innerHTML += `
-        <nav>
-        <label for="toggle">&#9776;</label>
-        <input type="checkbox" id="toggle" />
-        <!--   creación el menu -->
-        <div class="menu">
-            <a href="../index.html">Home</a>
-        </div>
-        </nav>
-    `
-    containerEstaticos.innerHTML = `
-        <div class="card backPetshop">
-            <h2>petshop</h2>
-            <div class="backImage"></div>
-            <a href="https://vuoso-l.github.io/Frontend1/petShop/" target="_blank">Ingresar</a>
-        </div>
-        <div class="card backHeroes">
-            <h2>súper héroes</h2>
-            <div class="backImage"></div>
-            <a href="https://vuoso-l.github.io/Frontend1/heroes/" target="_blank">Ingresar</a>
-        </div>
-        <div class="card backCvLucas">
-            <h2>CV - Lucas Vuoso</h2>
-            <div class="backImage"></div>
-            <a href="https://vuoso-l.github.io/curriculum_vitae/" target="_blank">Ingresar</a>
-        </div>
-        <div class="card backCvBatman">
-            <h2>CV - Batman</h2>
-            <div class="backImage"></div>
-            <a href="https://vuoso-l.github.io/Frontend1/cv_batman/" target="_blank">Ingresar</a>
-        </div>
-        <div class="card backLemonPie">
-            <h2>lemon pie</h2>
-            <div class="backImage"></div>
-            <a href="https://vuoso-l.github.io/Frontend1/lemon_pie/" target="_blank">Ingresar</a>
-        </div>
-        <div class="card backPizzeria">
-            <h2>pizzería</h2>
-            <div class="backImage"></div>
-            <a href="https://vuoso-l.github.io/Frontend1/pizza/" target="_blank">Ingresar</a>
-        </div>
+  e.preventDefault();
+  h1Estaticos.innerHTML = "Repositorio estático";
+  h2Estaticos.innerHTML = "";
+  h3Estaticos.innerHTML = "Basado en aprendizaje Certified Tech Developer";
+
+  containerEstaticos.innerHTML = `
+    <a class="btn-return" href="../index.html">Volver al Home</a>
+    <div class="card backPetshop">
+        <h2>petshop</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/Frontend1/petShop/" target="_blank">Ingresar</a>
+    </div>
+    <div class="card backHeroes">
+        <h2>súper héroes</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/Frontend1/heroes/" target="_blank">Ingresar</a>
+    </div>
+    <div class="card backCvLucas">
+        <h2>CV - Lucas Vuoso</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/curriculum_vitae/" target="_blank">Ingresar</a>
+    </div>
+    <div class="card backCvBatman">
+        <h2>CV - Batman</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/Frontend1/cv_batman/" target="_blank">Ingresar</a>
+    </div>
+    <div class="card backLemonPie">
+        <h2>lemon pie</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/Frontend1/lemon_pie/" target="_blank">Ingresar</a>
+    </div>
+    <div class="card backPizzeria">
+        <h2>pizzería</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/Frontend1/pizza/" target="_blank">Ingresar</a>
+    </div>
     `;
 });
--- js/proyectosReact.js
+++ js/proyectosReact.js
@@ -1,32 +1,27 @@
 const btnReact = document.querySelector(".btn-react");
-const headerReact = document.querySelector("header");
 const h1React = document.querySelector("h1");
+const h2React = document.querySelector("h2");
+const h3React = document.querySelector("h3");
 const containerReact = document.querySelector(".container");
 
 btnReact.addEventListener("click", (e) => {
-    console.log("click");
-    e.preventDefault();
-    h1React.innerHTML = "Repositorio React JS";
-    headerReact.innerHTML += `
-        <nav>
-        <label for="toggle">&#9776;</label>
-        <input type="checkbox" id="toggle" />
-        <!--   creación el menu -->
-        <div class="menu">
-            <a href="../index.html">Home</a>
-        </div>
-        </nav>
-    `
-    containerReact.innerHTML = `
-        <div class="card theme">
-            <h2>Cambio tema claro a oscuro</h2>
-            <div class="backImage"></div>
-            <a href=" https://vuoso-l.github.io/react_render-images/" target="_blank">Ingresar</a>
-        </div>        
-        <div class="card parcel-sandbox">
-            <h2>Renderizado imágenes base</h2>
-            <div class="backImage"></div>
-            <a href="https://github.com/vuoso-l/react_parcel-sandbox" target="_blank">Ingresar</a>
-        </div>
+  console.log("click");
+  e.preventDefault();
+  h1React.innerHTML = "Repositorio React JS";
+  h2React.innerHTML = "",
+  h3React.innerHTML = "",
+  
+  containerReact.innerHTML = `
+    <a class="btn-return" href="../index.html">Volver al Home</a>
+    <div class="card theme">
+        <h2>Cambio tema claro a oscuro</h2>
+        <div class="backImage"></div>
+        <a href=" https://vuoso-l.github.io/react_render-images/" target="_blank">Ingresar</a>
+    </div>        
+    <div class="card parcel-sandbox">
+        <h2>Renderizado imágenes base</h2>
+        <div class="backImage"></div>
+        <a href="https://github.com/vuoso-l/react_parcel-sandbox" target="_blank">Ingresar</a>
+    </div>
     `;
 });
