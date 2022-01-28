7244aa9a947ad27a9fe2b014e79505e3a6ba67c4
vuoso-l <vuosolucas@gmail.com>
2022-01-28 12:26:14 -0300

    Added new projects and changed header

--- js/proyectosReact.js
+++ js/proyectosReact.js
@@ -1,20 +1,15 @@
 const btnReact = document.querySelector(".btn-react");
-const h1React = document.querySelector("h1");
-const h2React = document.querySelector("h2");
-const h3React = document.querySelector("h3");
-const aReact = document.querySelector("a");
+const headerReact = document.querySelector(".headerIndex");
 const containerReact = document.querySelector(".container");
 
 btnReact.addEventListener("click", (e) => {
   console.log("click");
   e.preventDefault();
-  h1React.innerHTML = "Repositorio React JS";
-  h2React.innerHTML = "";
-  h3React.innerHTML = "";
-  
-  aReact.setAttribute("class", "btn-return");
-  aReact.setAttribute("href", "/index.html");
-  aReact.innerText = "Volver al home";
+
+  headerReact.innerHTML = `
+  <h1>Repositorio React JS</h1>
+  <a class="btn-return" href="./index.html">Volver al home</a>
+  `
 
   containerReact.innerHTML = `
     <div class="card theme">
@@ -26,6 +21,21 @@ btnReact.addEventListener("click", (e) => {
         <h2>Renderizado imágenes base</h2>
         <div class="backImage"></div>
         <a href="https://vuoso-l.github.io/react_parcel-sandbox/" target="_blank">Ingresar</a>
+    </div>
+    <div class="card crud">
+        <h2>Crud con API</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/react-crud-api_curso_jonMircha/" target="_blank">Ingresar</a>
+    </div>        
+    <div class="card music-search">
+        <h2>Buscador de canciones</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/react-buscador_canciones_curso_jonMircha/" target="_blank">Ingresar</a>
+    </div>
+    <div class="card modals">
+        <h2>Renderizado imágenes con ventana modal</h2>
+        <div class="backImage"></div>
+        <a href="https://vuoso-l.github.io/react-modals_curso_jonMircha-/" target="_blank">Ingresar</a>
     </div>
     `;
 });
